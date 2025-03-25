671378afe1b30a476468f1bfb137e8c8010f3c7f
danielCass <dcalves49@gmail.com>
2025-03-25 18:21:21 -0300

    feat: v1 true.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: LandingPageComponent,
+    },
+    {
+        path: 'FAQ',
+        component: LandingPageComponent,
+    },
+];
